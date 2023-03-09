9020d757b0458e9b4fe0a777feffbe3b6c579635
romeosanjose <romeosanjose@gmail.com>
2023-03-09 15:25:01 +0800

    add B2b changes

--- src/lib/context/account-context.tsx
+++ src/lib/context/account-context.tsx
@@ -1,9 +1,12 @@
+import axios from "axios"
+import {useRouter} from "next/router"
+import { MEDUSA_BACKEND_URL, medusaClient } from "@lib/config"
 import { medusaClient } from "@lib/config"
 import { Customer } from "@medusajs/medusa"
 import { useMutation } from "@tanstack/react-query"
 import { useMeCustomer } from "medusa-react"
 import { useRouter } from "next/router"
-import React, { createContext, useCallback, useContext, useState } from "react"
+import React, { createContext, useCallback, useContext, useState, useEffect } from "react"
 
 export enum LOGIN_VIEW {
   SIGN_IN = "sign-in",
@@ -17,12 +20,13 @@ interface AccountContext {
   checkSession: () => void
   refetchCustomer: () => void
   handleLogout: () => void
+  is_b2b: boolean
 }
 
 const AccountContext = createContext<AccountContext | null>(null)
 
 interface AccountProviderProps {
-  children?: React.ReactNode
+  children?: React.ReactNode 
 }
 
 const handleDeleteSession = () => {
@@ -30,6 +34,9 @@ const handleDeleteSession = () => {
 }
 
 export const AccountProvider = ({ children }: AccountProviderProps) => {
+  
+  const [is_b2b, setIsB2b] = useState(false)
+  
   const {
     customer,
     isLoading: retrievingCustomer,
@@ -42,7 +49,8 @@ export const AccountProvider = ({ children }: AccountProviderProps) => {
 
   const checkSession = useCallback(() => {
     if (!customer && !retrievingCustomer) {
-      router.push("/account/login")
+      // router.push("/account/login")
+      router.push(router.pathname.includes("wholesale") ? "/wholesale/account/login" : "/account/login")
     }
   }, [customer, retrievingCustomer, router])
 
@@ -57,9 +65,26 @@ export const AccountProvider = ({ children }: AccountProviderProps) => {
         remove()
         loginView[1](LOGIN_VIEW.SIGN_IN)
         router.push("/")
+        setIsB2b(false)
       },
     })
   }
+
+  const checkB2b = useCallback(async () => {
+    if (customer) {
+      //check if the customer is a b2b customer
+      const { data } = await axios.get(`${MEDUSA_BACKEND_URL}/store/customers/is-b2b`, {
+        withCredentials: true
+      })
+      setIsB2b(data.is_b2b)
+    } else {
+      setIsB2b(false)
+    }
+  },[customer])
+
+  useEffect(() => {
+    checkB2b()
+  }, [checkB2b])
 
   return (
     <AccountContext.Provider
@@ -70,6 +95,7 @@ export const AccountProvider = ({ children }: AccountProviderProps) => {
         checkSession,
         refetchCustomer: refetch,
         handleLogout,
+        is_b2b
       }}
     >
       {children}
